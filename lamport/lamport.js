5a9e9ca8d02e20fdccfb2b60f74e5d0dccfed540
Matheus Henrique <matheus.henrique@vtex.com>
2024-06-29 00:55:25 -0300

    create lamport class

---
+++ lamport/lamport.js
@@ -0,0 +1,18 @@
+const LamportClock = class LamportClock {
+  constructor() {
+    this.counter = 0;
+  }
+
+  increment() {
+    this.counter += 1
+  }
+
+  update(received_clock) {
+    this.counter = Math.max(this.counter, received_clock) + 1
+  }
+
+  getCounter() {
+    return this.counter
+  }
+
+}
